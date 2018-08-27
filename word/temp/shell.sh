78faf446565791493078ad6eba8a130b53381eda
yhong061 <yhong@actions-semi.com>
2018-08-27 17:48:11 +0800

    temp

---
+++ word/temp/shell.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+grep -nri ^$1a ./0.txt >> 1.txt
+grep -nri ^$1b ./0.txt >> 1.txt
+grep -nri ^$1c ./0.txt >> 1.txt
+grep -nri ^$1d ./0.txt >> 1.txt
+grep -nri ^$1e ./0.txt >> 1.txt
+grep -nri ^$1f ./0.txt >> 1.txt
+grep -nri ^$1g ./0.txt >> 1.txt
+grep -nri ^$1h ./0.txt >> 1.txt
+grep -nri ^$1i ./0.txt >> 1.txt
+grep -nri ^$1j ./0.txt >> 1.txt
+grep -nri ^$1k ./0.txt >> 1.txt
+grep -nri ^$1l ./0.txt >> 1.txt
+grep -nri ^$1m ./0.txt >> 1.txt
+grep -nri ^$1n ./0.txt >> 1.txt
+grep -nri ^$1o ./0.txt >> 1.txt
+grep -nri ^$1p ./0.txt >> 1.txt
+grep -nri ^$1q ./0.txt >> 1.txt
+grep -nri ^$1r ./0.txt >> 1.txt
+grep -nri ^$1s ./0.txt >> 1.txt
+grep -nri ^$1t ./0.txt >> 1.txt
+grep -nri ^$1u ./0.txt >> 1.txt
+grep -nri ^$1v ./0.txt >> 1.txt
+grep -nri ^$1w ./0.txt >> 1.txt
+grep -nri ^$1x ./0.txt >> 1.txt
+grep -nri ^$1y ./0.txt >> 1.txt
+grep -nri ^$1z ./0.txt >> 1.txt
+
+echo "" >> 1.txt
