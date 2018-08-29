de3ce425732974e63453000a3d31c3968bf06148
yhong061 <yhong@actions-semi.com>
2018-08-29 15:45:34 +0800

    verb

---
+++ word/temp/all.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+echo "$1"
+source shell.sh $1a
+source shell.sh $1b
+source shell.sh $1c
+source shell.sh $1d
+source shell.sh $1e
+source shell.sh $1f
+source shell.sh $1g
+source shell.sh $1h
+source shell.sh $1i
+source shell.sh $1j
+source shell.sh $1k
+source shell.sh $1l
+source shell.sh $1m
+source shell.sh $1n
+source shell.sh $1o
+source shell.sh $1p
+source shell.sh $1q
+source shell.sh $1r
+source shell.sh $1s
+source shell.sh $1t
+source shell.sh $1u
+source shell.sh $1v
+source shell.sh $1w
+source shell.sh $1x
+source shell.sh $1y
+source shell.sh $1z
+
+
+
+
---
+++ word/temp/ball.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+source all.sh a
+source all.sh b
+source all.sh c
+source all.sh d
+source all.sh e
+source all.sh f
+source all.sh g
+source all.sh h
+source all.sh i
+source all.sh j
+source all.sh k
+source all.sh l
+source all.sh m
+source all.sh n
+source all.sh o
+source all.sh p
+source all.sh q
+source all.sh r
+source all.sh s
+source all.sh t
+source all.sh u
+source all.sh v
+source all.sh w
+source all.sh x
+source all.sh y
+source all.sh z
+
+
+
+
